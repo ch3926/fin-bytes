21f1b09ecf263c76339c3d9ff4a442e75043830a
ch3926 <ch3926@nyu.edu>
2023-12-03 16:27:53 -0500

    Turn sheet data into csv for emailing use.

--- get_user_data.py
+++ get_user_data.py
@@ -1,5 +1,6 @@
 import gspread
 import json
+import csv
 
 sa = gspread.service_account(filename="fin-bytes.json") # service account to use for api
 sh = sa.open("fin-bytes") # the google sheet containing all the user entered data
@@ -15,3 +16,13 @@ sheet_list = sheet.get_all_values()
 # convert list of dict values into json
 # json_info = json.dumps(sheet_dict)
 # print(json_info)
+
+
+# convert list of dicts to csv
+
+keys = sheet_dict[0].keys()
+
+with open('user_info.csv', 'w', newline='') as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(sheet_dict)
