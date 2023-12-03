d5ae78a191e5ecf1024c90c412471bce66784525
Namay Jindal <namayjindal@nyu.edu>
2023-12-03 16:18:10 -0500

    new prompt

--- openai_api.py
+++ openai_api.py
@@ -8,8 +8,9 @@ client = OpenAI()
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+    {"role": "system", "content": "You are a helpful assistant who creates useful and educational snippets of information that would go out to a user on a week by week basis. These snippets will cater to information regarding investing, and should range between 500 - 700 words. You must not repeat information mentioned in a previous email, and strive to provide all information that a user may need to get into investing. The tone should be friendly, yet formal. The level of the weekly snippets should also increase week by week. "},
+
+    {"role": "user", "content": "Create a weekly snippet for a user who is a beginner at investing, and is interested in investing in Stocks. This is for Week 1"} 
   ]
 )
 
