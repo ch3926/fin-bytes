c92e754f12dfaae02cc2f25174c771cad5faf5d5
ch3926 <ch3926@nyu.edu>
2023-12-03 15:52:10 -0500

    Convert to json. Added comments.

--- get_user_data.py
+++ get_user_data.py
@@ -1,12 +1,17 @@
 import gspread
+import json
 
-sa = gspread.service_account(filename="fin-bytes.json")
-sh = sa.open("fin-bytes")
+sa = gspread.service_account(filename="fin-bytes.json") # service account to use for api
+sh = sa.open("fin-bytes") # the google sheet containing all the user entered data
+sheet = sh.worksheet("Investing Survey") # specific sheet containing the data
 
-sheet = sh.worksheet("Investing Survey")
-
+# grab user entered records from spreadsheet so we can use them here
 sheet_dict = sheet.get_all_records()
 sheet_list = sheet.get_all_values()
 
 #print(sheet_dict)
-print(sheet_list)
+#print(sheet_list)
+
+# convert list of dict values into json
+# json_info = json.dumps(sheet_dict)
+# print(json_info)
