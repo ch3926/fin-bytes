e8aeed25f5f888e12f74f30ea5c8f91c44f78887
Namay Jindal <namayjindal@nyu.edu>
2023-12-03 19:47:33 -0500

    last commit

--- main.py
+++ main.py
@@ -8,7 +8,36 @@ import csv
 import ssl
 import smtplib
 
+import gspread
+import json
+import csv
+import smtplib
+import ssl
 
+
+sa = gspread.service_account(filename="fin-bytes.json") # service account to use for api
+sh = sa.open("fin-bytes") # the google sheet containing all the user entered data
+sheet = sh.worksheet("Investing Survey") # specific sheet containing the data
+
+# grab user entered records from spreadsheet so we can use them here
+sheet_dict = sheet.get_all_records()
+sheet_list = sheet.get_all_values()
+
+#print(sheet_dict)
+#print(sheet_list)
+
+# convert list of dict values into json
+# json_info = json.dumps(sheet_dict)
+# print(json_info)
+
+# convert list of dicts to csv
+
+keys = sheet_dict[0].keys()
+
+with open('user_info.csv', 'w', newline='') as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(sheet_dict)
 # Init
 newsapi = NewsApiClient(api_key='01246e43061d4f3f981373160e2b4ec5')
 
